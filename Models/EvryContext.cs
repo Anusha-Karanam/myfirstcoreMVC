310f6c4efa0f514bc7c5dfc6f8eeba11e38b5f57
Anusha-Karanam <anushakaranambest@gmail.com>
2022-10-12 22:01:14 +0530

    commit12/10

--- Models/EvryContext.cs
+++ Models/EvryContext.cs
@@ -25,7 +25,7 @@ namespace myfirstcoreMVC.Models
 //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
          //       optionsBuilder.UseSqlServer("Server=ELW5234\\SQLEXPRESS;Database=Evry;Trusted_Connection=True;");
          //   }
-       // }
+       //// }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
