b724bfdbfdf1d7768724fdfef7cf5db6d562ddc5
Anusha-Karanam <anushakaranambest@gmail.com>
2022-10-12 22:00:27 +0530

    Add project files.

---
+++ Models/Trainee.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace myfirstcoreMVC.Models
+{
+    public partial class Trainee
+    {
+        public int Tid { get; set; }
+        public string? Name { get; set; }
+        public int? Tage { get; set; }
+    }
+}
